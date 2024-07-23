6573aea044a70f8140328e749cc1ef94e7bfea92
Joaquin Cuello <joacoide.cuello@gmail.com>
2024-07-23 17:33:52 -0300

    confg 1 main.java .gitignore

---
+++ src/javathings/Administrador.java
@@ -0,0 +1,12 @@
+package javathings;
+
+public class Administrador {
+	public int idAdmin;
+	
+	public void administrador(int idAdmin) {
+		this.idAdmin = idAdmin;
+		
+		System.out.println(idAdmin);
+	};
+	
+}
